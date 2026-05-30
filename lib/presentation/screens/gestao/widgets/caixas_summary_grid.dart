4d50ec242b88ff280f59dc3d34fb9547ab6b998f
unyxapp-bit <unyx.app@gmail.com>
2026-05-30 14:28:57 -0300

    refactor: adiciona estrutura inicial summary grid

---
+++ lib/presentation/screens/gestao/widgets/caixas_summary_grid.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CaixasSummaryGridPlaceholder extends StatelessWidget {
+  const CaixasSummaryGridPlaceholder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox.shrink();
+  }
+}
